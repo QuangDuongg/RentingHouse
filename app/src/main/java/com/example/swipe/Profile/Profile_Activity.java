da0f9f5af8b4cf11e73946e439656d7af152a2ff
QuangDuongg <22125081@student.hcmus.edu.vn>
2024-08-30 00:02:12 +0700

    Edit Profile
    - Last step, add the Image to Firebase

--- app/src/main/java/com/example/swipe/Profile/Profile_Activity.java
+++ app/src/main/java/com/example/swipe/Profile/Profile_Activity.java
@@ -8,19 +8,97 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.swipe.Main.Cards;
+import com.example.swipe.Main.ProfileCheckinMain;
 import com.example.swipe.R;
 
+import android.content.Context;
+import android.content.Intent;
+import androidx.annotation.NonNull;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.swipe.Utils.PulsatorLayout;
+import com.example.swipe.Utils.TopNavigationViewHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
+
+
 public class Profile_Activity extends AppCompatActivity {
+    private static final String TAG = "Profile_Activity";
+    private static final int ACTIVITY_NUM = 0;
+    static boolean active = false;
+
+    private Context mContext = Profile_Activity.this;
+    private ImageView imagePerson;
+    private TextView name;
+
+    private String userId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: create the page");
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        PulsatorLayout mPulsator = findViewById(R.id.pulsator);
+        mPulsator.start();
+
+        setupTopNavigationView();
+
+        imagePerson = findViewById(R.id.circle_profile_image);
+        name = findViewById(R.id.profile_name);
+
+
+        ImageButton edit_btn = findViewById(R.id.edit_profile);
+        edit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile_Activity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile_Activity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: resume to the page");
+
+    }
+
+
+    private void setupTopNavigationView() {
+        Log.d(TAG, "setupTopNavigationView: setting up TopNavigationView");
+        BottomNavigationView tvEx = findViewById(R.id.topNavViewBar);
+        TopNavigationViewHelper.setupTopNavigationView(tvEx);
+        TopNavigationViewHelper.enableNavigation(mContext, tvEx);
+        Menu menu = tvEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+
+
+
 }
