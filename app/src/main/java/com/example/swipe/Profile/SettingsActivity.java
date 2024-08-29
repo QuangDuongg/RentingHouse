b2a92c55fc2fae8a0e7ad3649386d2af59b4dedb
QuangDuongg <22125081@student.hcmus.edu.vn>
2024-08-30 00:26:18 +0700

    Ok about UI
    * Lack of:
    - Near which school
    - Location

--- app/src/main/java/com/example/swipe/Profile/SettingsActivity.java
+++ app/src/main/java/com/example/swipe/Profile/SettingsActivity.java
@@ -8,19 +8,90 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.swipe.Introduction.IntroductionMain;
 import com.example.swipe.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
 public class SettingsActivity extends AppCompatActivity {
+    private static final String TAG = "SettingsActivity";
+    SeekBar distance;
+    SwitchCompat man, woman;
+    TextView gender, distance_text, age_rnge;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        TextView toolbar = findViewById(R.id.toolbartag);
+        toolbar.setText("Profile");
+        ImageButton back = findViewById(R.id.back);
+        distance = findViewById(R.id.distance);
+        man = findViewById(R.id.switch_man);
+        woman = findViewById(R.id.switch_woman);
+        distance_text = findViewById(R.id.distance_text);
+
+        distance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                distance_text.setText(progress + " Km");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
         });
+
+        man.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    man.setChecked(true);
+                }
+            }
+        });
+        woman.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    woman.setChecked(true);
+                }
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
     }
+
+    public void Logout(View view) {
+        startActivity(new Intent(getApplicationContext(), IntroductionMain.class));
+        finish();
+
+    }
+
+
 }
